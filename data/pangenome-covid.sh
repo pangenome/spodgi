afbdef9d236040a65fed36ebaa7d48cba5d95324
Jerven bolleman <jerven.bolleman@sib.swiss>
2020-04-28 10:56:04 +0200

    A lot less data munging is needed now that spodgi keeps url path names as iri

--- data/pangenome-covid.sh
+++ data/pangenome-covid.sh
@@ -1,73 +1,104 @@
 #!/usr/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../covid/"
 source ${DIR}/../env.sh
+download=$1
+baseIri="http://arvados.org/"
+if [ $download -eq 1 ];
+then
 
-# Get the latest odgi from the virtual biohackathon build
-wget "https://workbench.lugli.arvadosapi.com/collections/ed4277a320bd66fc1527142a9ad9ac2e+284/readsMergeDedup.odgi?disposition=attachment&size=601574" -O ${DIR}pangenome-covid.odgi
+    # Get the latest odgi from the virtual biohackathon build
+    #wget "https://workbench.lugli.arvadosapi.com/collections/lugli-4zz18-z513nlpqm03hpca/readsMergeDedup.ttl.xz" -O -| \
+    #  xzcat | \
+    #  sed -e 's_http://example.org/vg/path/__g' | \
+    #  sed -e 's_http://example.org/vg/_https://workbench.lugli.arvadosapi.com/collections/lugli-4zz18-z513nlpqm03hpca/_g' > "${DIR}/pangenome-covid.ttl"
 
-baseIri="https://graph-genome.github.io/SARS2-CoV2-genbank/$(date --iso)/"
+    wget -N "https://workbench.lugli.arvadosapi.com/collections/lugli-4zz18-z513nlpqm03hpca/readsMergeDedup.odgi" -O ${DIR}/pangenome-covid.odgi ;
+    ${DIR}/../odgi_to_rdf.py ${DIR}/pangenome-covid.odgi - > "${DIR}/pangenome-covid.ttl"
 
-# Covert this to basic rdf ntriples
-${DIR}/../odgi_to_rdf.py --base "$baseIri" ${DIR}/pangenome-covid.odgi ${DIR}/pangenome-covid.nt
 
-# Lets start fixing IRI's and making somewhat nice turtle
-cat <(echo -e  "@base <$baseIri> .") \
- <(echo "@prefix vg: <http://biohackathon.org/resource/vg#> .") \
- <(echo "@prefix f2f: <http://biohackathon.org/resource/vg#linksForwardToForward> .") \
- <(echo "@prefix N: <http://biohackathon.org/resource/vg#Node> .") \
- <(echo "@prefix l: <http://biohackathon.org/resource/vg#links> .") \
- <(echo "@prefix v: <http://www.w3.org/1999/02/22-rdf-syntax-ns#value> .") \
- <(echo "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .") \
- <(echo "@prefix faldo: <http://biohackathon.org/resource/faldo#> .") \
- <(echo "@prefix EP: <http://biohackathon.org/resource/faldo#ExactPosition> .") \
- <(echo "@prefix i: <http://identifiers.org/indsc/> .") \
- <(echo "@prefix owl: <http://www.w3.org/2002/07/owl#> .") \
- <(echo "@prefix P: <http://biohackathon.org/resource/faldo#Position> .") \
- ${DIR}/pangenome-covid.nt | rapper -i turtle -I "$baseIri" -o turtle - \
- > ${DIR}/pangenome-covid.ttl
 
-cd $DIR
-mkdir -p insdc
-cd insdc
-# One wget invocation saves a lot of time because of reusing the http connection
-# We look for identifiers in the pangenome file to hook onto
-wget -c -i <(grep -P "^<path\/[A-Z_0-9]+\/" $DIR/pangenome-covid.ttl | \
-             cut -f 2 -d '/' | \
+    wget -N "https://workbench.lugli.arvadosapi.com/collections/lugli-4zz18-z513nlpqm03hpca/mergedmetadata.ttl" -O "${DIR}/pangenome-metadata-raw.ttl"
+grep -v 'http://purl.obolibrary.org/obo/NCIT_C42781' "${DIR}/pangenome-metadata-raw.ttl" > "${DIR}/pangenome-metadata.ttl"
+grep "http://purl.obolibrary.org/obo/NCIT_C42781" "${DIR}/pangenome-metadata-raw.ttl" | \
+    sed -e 's_.;_.","_g' | \
+    sed -e 's_,_, _g' >> "${DIR}/pangenome-metadata.ttl" 
+
+fi
+    #baseIri="https://graph-genome.github.io/SARS2-CoV2-genbank/$(date --iso)/"
+    # Covert this to basic rdf ntriples
+    #${DIR}/../odgi_to_rdf.py --base "$baseIri" ${DIR}/pangenome-covid.odgi ${DIR}/pangenome-covid.nt
+
+    #curl "https://workbench.lugli.arvadosapi.com/collections/1d61bf7c5d000d5e51e41e2f10e2896d+408/readsMergeDedup.ttl.xz?disposition=attachment&size=8748" | unxz > ${DIR}/pangenome-covid.nt
+
+    # Lets start fixing IRI's and making somewhat nice turtle
+    cat <(echo "@base <$baseIri> .") \
+        <(echo "@prefix vg: <http://biohackathon.org/resource/vg#> .") \
+        <(echo "@prefix f2f: <http://biohackathon.org/resource/vg#linksForwardToForward> .") \
+        <(echo "@prefix N: <http://biohackathon.org/resource/vg#Node> .") \
+        <(echo "@prefix l: <http://biohackathon.org/resource/vg#links> .") \
+        <(echo "@prefix v: <http://www.w3.org/1999/02/22-rdf-syntax-ns#value> .") \
+        <(echo "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .") \
+        <(echo "@prefix faldo: <http://biohackathon.org/resource/faldo#> .") \
+        <(echo "@prefix EP: <http://biohackathon.org/resource/faldo#ExactPosition> .") \
+        <(echo "@prefix i: <http://identifiers.org/indsc/> .") \
+        <(echo "@prefix s: <http://www.w3.org/2004/02/skos/core#> .") \
+        <(echo "@prefix owl: <http://www.w3.org/2002/07/owl#> .") \
+        <(echo "@prefix P: <http://biohackathon.org/resource/faldo#Position> .") \
+        ${DIR}/pangenome-covid.ttl | \
+        riot --syntax=turtle --output=turtle --base "$baseIri" > ${DIR}/pangenome-covid-nice.ttl
+        #rapper -i turtle -I "$baseIri" -o turtle - \
+
+if [ $download -eq 1 ]
+then
+    cd $DIR
+    mkdir -p insdc
+    cd insdc
+
+    # One wget invocation saves a lot of time because of reusing the http connection
+    #We look for identifiers in the pangenome file to hook onto
+    wget -N -i <(grep "http://biohackathon.org/bh20-seq-schema/original_fasta_label" "${DIR}/pangenome-metadata.ttl"  | \
+    cut -f 2 -d '"' | \
              sort -u | \
              xargs -I '{}' -exec echo "http://togows.org/entry/nucleotide/{}.ttl")
+fi
 cd $DIR
 
 #Concatenate all these INSDC ttl files into one
-echo "@prefix : <$baseIri> . " > $DIR/insdc.ttl
-rapper -i turtle -o turtle <(cat $DIR/insdc/*.ttl) >> $DIR/insdc.ttl
+echo "Gathering all INSDC files"
+rapper -i turtle -o turtle <(cat <(echo '@prefix i: <http://identifiers.org/insdc/> .') $DIR/insdc/*.ttl) > $DIR/insdc.ttl
 
-forsed=""
-# Replace path IRIs with INSDC IRIs 
-# Also add owl:sameAs to some uniprot/embl IRIs
-for path in $(grep -P "^<path\/[A-Z_0-9]+\/" $DIR/pangenome-covid.ttl | \
-             cut -f 2 -d '/' | \
-             sort -u )
-do
-    echo $path
-    sequence=$(grep -m 1 -oP "$path\.\d+#sequence" $DIR/insdc.ttl)
-    forsed="$forsed -e 's|path/${path}|http://identifiers.org/insdc/${sequence}|g'"
-    echo "<http://identifiers.org/insdc/$sequence> owl:sameAs <http://purl.uniprot.org/embl/$path> ." >> $DIR/pangenome-covid.ttl
+echo "Adding owl:sameAs and skos:closeMatch"
+echo "@prefix skos: <http://www.w3.org/2004/02/skos/core#> ." >> "${DIR}/pangenome-covid-nice.ttl"
+echo "@prefix owl: <http://www.w3.org/2002/07/owl#> ." >> "${DIR}/pangenome-covid-nice.ttl"
+grep "http://biohackathon.org/bh20-seq-schema/original_fasta_label" "${DIR}/pangenome-metadata.ttl" | sed -e 's|<http://biohackathon.org/bh20-seq-schema/original_fasta_label>|skos:closeMatch|g' | sed -E 's|\"([A-Z0-9]+.[0-9]+)\"|<http://identifiers.org/insdc/\1#sequence>|g' >> "${DIR}/pangenome-covid-nice.ttl"
 
-    echo "<http://purl.uniprot.org/embl/$path> owl:sameAs <http://identifiers.org/insdc/$sequence> ." >> $DIR/pangenome-covid.ttl
-done
+grep "http://biohackathon.org/bh20-seq-schema/original_fasta_label" "${DIR}/pangenome-metadata.ttl" | sed -e 's|<http://biohackathon.org/bh20-seq-schema/original_fasta_label>|skos:closeMatch|g' | sed -E 's|\"([A-Z0-9]+).[0-9]+\"|<http://purl.uniprot.org/embl/\1>|g' >> "${DIR}/pangenome-covid-nice.ttl"
 
-# Run one sed invocation instead of lots with
-echo $forsed
-conv="cat $DIR/pangenome-covid.ttl | sed $forsed > $DIR/pangenome-covid.ttl.test"
-eval $conv
-mv $DIR/pangenome-covid.ttl.test $DIR/pangenome-covid.ttl
+grep "http://biohackathon.org/bh20-seq-schema/original_fasta_label" "${DIR}/pangenome-metadata.ttl" | sed -e 's|<http://biohackathon.org/bh20-seq-schema/original_fasta_label>|skos:closeMatch|g' | sed -E 's|.+ \"([A-Z0-9]+).([0-9]+)\"|<http://purl.uniprot.org/embl/\1> owl:sameAs <http://identifiers.org/insdc/\1.\2#sequence> |g' >> "${DIR}/pangenome-covid-nice.ttl"
 
+grep "http://biohackathon.org/bh20-seq-schema/original_fasta_label" "${DIR}/pangenome-metadata.ttl" | sed -e 's|<http://biohackathon.org/bh20-seq-schema/original_fasta_label>|skos:closeMatch|g' | sed -E 's|.+ \"([A-Z0-9]+).([0-9]+)\"|<http://identifiers.org/insdc/\1.\2#sequence> owl:sameAs <http://purl.uniprot.org/embl/\1> |g' >> "${DIR}/pangenome-covid-nice.ttl"
 
-# Concatentate into one and do a basic compression step
-rapper -i turtle \
-       -o turtle \
-       -I "http://identifiers.org/insdc/" \
-       <(cat $DIR/insdc.ttl $DIR/pangenome-covid.ttl) | \
+echo "Concatentate into one and do a basic compression step"
+
+cat \
+    <(echo "@prefix vg: <http://biohackathon.org/resource/vg#> .") \
+    <(echo "@prefix f2f: <http://biohackathon.org/resource/vg#linksForwardToForward> .") \
+    <(echo "@prefix N: <http://biohackathon.org/resource/vg#Node> .") \
+    <(echo "@prefix l: <http://biohackathon.org/resource/vg#links> .") \
+    <(echo "@prefix v: <http://www.w3.org/1999/02/22-rdf-syntax-ns#value> .") \
+    <(echo "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .") \
+    <(echo "@prefix faldo: <http://biohackathon.org/resource/faldo#> .") \
+    <(echo "@prefix EP: <http://biohackathon.org/resource/faldo#ExactPosition> .") \
+    <(echo "@prefix i: <http://identifiers.org/indsc/> .") \
+    <(echo "@prefix s: <http://www.w3.org/2004/02/skos/core#> .") \
+    <(echo "@prefix owl: <http://www.w3.org/2002/07/owl#> .") \
+    <(echo "@prefix P: <http://biohackathon.org/resource/faldo#Position> .") \
+    <(echo "@prefix EP: <http://biohackathon.org/resource/faldo#ExactPosition> .") \
+    <(echo "@prefix i: <http://identifiers.org/indsc/> .") \
+    <(echo "@prefix s: <http://www.w3.org/2004/02/skos/core#> .") \
+    <(echo "@prefix owl: <http://www.w3.org/2002/07/owl#> .") > $DIR/pangenome-covid-all.ttl
+riot --syntax=turtle --output=turtle --base "$baseIri"  \
+       <(cat $DIR/insdc.ttl $DIR/pangenome-covid-nice.ttl $DIR/pangenome-metadata.ttl) | \
  grep -v "^$" | \
  sed 's/vg:linksForwardToForward/f2f:/g' | \
  sed 's/vg:Node/N:/g' | \
@@ -75,4 +106,6 @@ rapper -i turtle \
  sed 's/faldo:ExactPosition/EP:/g' | \
  sed 's/faldo:Position/P:/g' | \
  sed 's/rdf:value/v:/g' \
- > $DIR/$(date --iso|tr -d '-').ttl
+ >> $DIR/pangenome-covid-all.ttl 
+cp $DIR/pangenome-covid-all.ttl $DIR/$(date --iso|tr -d '-').ttl
+
